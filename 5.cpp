6d2dd2898a7e0892d58c25d6272bbb7aac9b7341
Jayant <jayantawasthi5@gmail.com>
2022-02-08 20:40:56 +0530

    Course Schedule

---
+++ 5.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool checkCycle(int node,vector<int> adj[],int vis[],int dfsVis[])
+    {
+        vis[node]=1;
+        dfsVis[node]=1;
+        for(auto it:adj[node])
+        {
+            if(!vis[it])
+            {
+                if(checkCycle(it,adj,vis,dfsVis))
+                    return true;
+            }
+            else if(dfsVis[it])
+            {
+                return true;
+            }
+        }
+        dfsVis[node]=0;
+        return false;
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        if(prerequisites.size()==1)
+        {
+            return true;
+        }
+        vector<int> adj[numCourses];
+        for(int i=0;i<prerequisites.size();i++)
+        {
+            adj[prerequisites[i][0]].push_back(prerequisites[i][1]);
+        }
+        int vis[numCourses],dfsVis[numCourses];
+        memset(vis,0,sizeof vis);
+        memset(dfsVis,0,sizeof dfsVis);
+        for(int i=0;i<numCourses;i++)
+        {
+            if(!vis[i])
+            {
+                if(checkCycle(i,adj,vis,dfsVis))
+                    return false;
+            }
+        }
+        return true;
+    }
+};
